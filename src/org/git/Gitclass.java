cec129143fcbb48e2e0abe6221bc9ffa6b936a93
pugazhenthi07 <mail.pugazhenthi07@gmail.com>
2022-03-08 22:16:55 +0530

    pugazh

---
+++ src/org/git/Gitclass.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class Gitclass {
+	private void method1() {
+        System.out.println("1");
+}
+private void method2() {
+       System.out.println("2");
+}
+private void method3() {
+       System.out.println("3");
+}
+
+}
